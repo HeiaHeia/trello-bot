9d81728e74ddeb51233250633f92f8a693f6f1de
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-10-10 17:51:54 +0300

    Add configuration handling

---
+++ config.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+type Config struct {
+	TrelloUser       string `json:"trello-user"`
+	TrelloKey        string `json:"trello-key"`
+	TrelloToken      string `json:"trello-token"`
+	BoardName        string `json:"board-name"`
+	StartListName    string `json:"start-list-name"`
+	FinishedListName string `json:"finished-list-name"`
+	Port             string `json:"port"`
+}
+
+func LoadConfig(filename string) Config {
+
+	var config Config
+
+	file, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Printf("Error reading config file: %v\n", err)
+	}
+
+	json.Unmarshal(file, &config)
+
+	return config
+}
