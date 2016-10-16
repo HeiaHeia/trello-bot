08fc2beb3cd19ff79d20b072b7f124708cddc29e
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-10-16 18:31:22 +0300

    Add methods to send private messages

---
+++ slack/dm.go
@@ -0,0 +1,32 @@
+package slack
+
+import (
+	"github.com/nlopes/slack"
+)
+
+func DmUpdate(name string, message string) {
+
+	username, err := parseUsername(name)
+	if err != nil {
+		return
+	}
+
+	user, err := getUserWithUsername(username)
+	if err != nil {
+		return
+	}
+
+	err = sendDm(user, message)
+}
+
+func sendDm(user slack.User, message string) error {
+
+	_, _, channel, err := slackClient.OpenIMChannel(user.ID)
+	if err != nil {
+		return err
+	}
+
+	_, _, err = slackClient.PostMessage(channel, message, slack.PostMessageParameters{})
+
+	return err
+}
