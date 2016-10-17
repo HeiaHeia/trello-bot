0399039971b0acdb437529ef782bc5501bba1f17
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-10-17 13:36:39 +0300

    Start webhook listen server

--- main.go
+++ main.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"trello-bot/slack"
+	"trello-bot/trello"
 )
 
 func main() {
 
 	config := LoadConfig("config.json")
-	fmt.Println(config)
+
+	trello.Authenticate(trello.TrelloConfig{
+		Key:              config.TrelloKey,
+		Token:            config.TrelloToken,
+		User:             config.TrelloUser,
+		Board:            config.BoardName,
+		StartingListName: config.StartListName,
+		FinishedListName: config.FinishedListName})
+
+	slack.Authenticate(config.SlackToken)
+
+	go trello.SetupWebhooks(config.ListenURL)
+	http.HandleFunc("/trello_webhook", trello.WebhookHandler)
+	listen := fmt.Sprintf(":%v", config.Port)
+	fmt.Println("Starting server...")
+	http.ListenAndServe(listen, nil)
 }
