a3271349e147e4693a23a85cbf1683e6ee64776c
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2016-12-19 14:37:21 +0200

    Set up webhooks

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/aodin/date"
 	"net/http"
+	"time"
 	"trello-bot/slack"
 	"trello-bot/trello"
 )
@@ -15,14 +16,18 @@ func main() {
 	globalConfig = LoadConfig("config.json")
 
 	trello.Authenticate(trello.TrelloConfig{Key: globalConfig.TrelloKey, Token: globalConfig.TrelloToken, User: globalConfig.TrelloUser, ActionHandler: ActionHandler})
-
 	slack.Authenticate(globalConfig.SlackToken)
 
-	go trello.SetupWebhooks(globalConfig.ListenURL)
+	for i := range globalConfig.BoardConfigs {
+		boardName := globalConfig.BoardConfigs[i].BoardName
+		go setupWebhook(boardName)
+	}
 	http.HandleFunc("/trello_webhook", trello.WebhookHandler)
 	uid := RandomizeUID()
 	err := slack.TryMessageChannelName(globalConfig.InfoChannel, fmt.Sprint("Trellobot report URL is ", globalConfig.ListenURL+"/"+uid))
-	fmt.Println("Error sending report URL:", err)
+	if err != nil {
+		fmt.Println("Error sending report URL:", err)
+	}
 	http.HandleFunc("/"+uid, reportHandler)
 	listen := fmt.Sprintf(":%v", globalConfig.Port)
 	fmt.Println("Starting server...")
@@ -38,4 +43,11 @@ func reportHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	fmt.Fprintln(w, report)
+}
+
+func setupWebhook(boardName string) {
+
+	fmt.Println("Waiting for the server to start up before creating webhooks")
+	time.Sleep(2 * time.Second)
+	trello.SetupWebhook(boardName, globalConfig.ListenURL)
 }
