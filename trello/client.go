3e977900f22710a4782c5092fcf852b860acfe0a
Joonas Myhrberg <joonas.myhrberg@gmail.com>
2017-01-03 22:36:19 +0200

    Add global trelloUser variable

--- trello/client.go
+++ trello/client.go
@@ -5,13 +5,23 @@ import (
 )
 
 var trelloClient *trello.Client
+var trelloUser *trello.Member
 
 func Setup(config TrelloConfig) error {
 
 	trelloConfig = config
 
 	trello, err := trello.NewAuthClient(config.Key, &config.Token)
+	if err != nil {
+		return err
+	}
 	trelloClient = trello
 
-	return err
+	user, err := trello.Member(config.User)
+	if err != nil {
+		return err
+	}
+	trelloUser = user
+
+	return nil
 }
